ef41da65cbd1b44ae61b12d4286118e1bc1ad72c
Sherdos <sherdos26@gmail.com>
2024-04-19 17:57:08 +0600

    add class views

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from product import views as v
 urlpatterns = [
-    path('', v.index, name='index'),
+    path('', v.IndexView.as_view(), name='index'),
     path('products/', v.products, name='products'),
-    path('product/show/<int:pk>', v.show_product, name='show_product'),
+    path('product/show/<int:pk>', v.ShowProductView.as_view(), name='show_product'),
     path('product/search/', v.search, name='search'),
     path('login/', v.login_user, name='login'),
     path('register/', v.register_user, name='register'),
--- product/views.py
+++ product/views.py
@@ -5,34 +5,40 @@ from product.models import Product
 from product.utils import search_product
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
+from django.views import generic
 
 # Create your views here.
 
-def index(request):
-    template_name = 'pages/index.html'
-    cards = Product.objects.all().order_by('id').reverse()[:3]
-    context = {
-        'title':"Главная",
-        'cards':cards
-    }
-    return render(request, template_name,context)
+# def index(request):
+#     template_name = 'pages/index.html'
+#     cards = Product.objects.all().order_by('id').reverse()[:3]
+#     context = {
+#         'title':"Главная",
+#         'cards':cards
+#     }
+#     return render(request, template_name,context)
 
-def show_product(request, pk):
+class IndexView(generic.ListView):
+    model = Product
+    template_name = 'pages/index.html'
+    context_object_name = 'cards'
+
+
+# def show_product(request, pk):
+#     template_name = 'pages/show_product.html'
+#     card = Product.objects.get(id=pk)
+#     if request.method == 'POST':
+#         form = Review(request.POST)
+#         if form.is_valid():
+#             review_num = int(form.data['review'])
+#             card.rating += review_num
+
+class ShowProductView(generic.DetailView, generic.CreateView):
+    model = Product
     template_name = 'pages/show_product.html'
-    card = Product.objects.get(id=pk)
-    if request.method == 'POST':
-        form = Review(request.POST)
-        if form.is_valid():
-            review_num = int(form.data['review'])
-            card.rating += review_num
-            
-    form_review = Review()
-    context = {
-        'title':f'{card.title}',
-        'card':card,
-        'form_review':form_review
-    }
-    return render(request, template_name,context)
+    context_object_name = 'card'
+    form_class = Review
+
 
 def products(request):
     template_name = 'pages/clothes.html'
